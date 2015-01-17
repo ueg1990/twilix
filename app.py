7b165a65f35338d1f8468f08a6b59c30cc7d8c7a
ueg1990 <uehtesham90@gmail.com>
2015-01-17 15:31:20 -0500

    Add code for forms

--- app.py
+++ app.py
@@ -1,18 +1,55 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request 
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
+from forms import UserRegistrationForm 
 
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 db = SQLAlchemy(app)
 db.create_all()
 
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    server_url = db.Column(db.String(120), index=True, unique=True)
+    server_password = db.Column(db.String(24))
+    email = db.Column(db.String(120), index=True, unique=True)
+    twilio_number = db.Column(db.String(60), index=True, unique=True)
+
+    def __init__(self, email, server_url, server_password,twilio_number):
+        self.email = email
+        self.server_url = server_url
+        self.server_password = server_password
+        self.twilio_number = self._format_number(twilio_number)
+
+    def _format_number(self, number):
+        if number[:2] != '+1':
+            return '+1' + number
+        return number
+
+    def __repr__(self):
+        return '<User %r>' % (self.email)
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/bye/")
+def bye():
+    return "Bye World!!!"
+
+@app.route("/register/", methods=['GET', 'POST'])
+def register():
+    form = UserRegistrationForm()
+    if request.method == 'POST' and form.validate_on_submit():
+	return redirect(url_for('bye'))
+    return render_template('register.html', form=form)
+
 
 if __name__ == "__main__":
     app.debug = True
---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class UserRegistrationForm(Form):
+    email = StringField('email', validators=[DataRequired()])
+    server_url = StringField('server_url', validators=[DataRequired()])
+    server_password = StringField('server_password', validators=[DataRequired()])
+    twilio_number = StringField('twilio_number', validators=[DataRequired()])
