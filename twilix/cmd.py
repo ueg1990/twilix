5d634511af87150cf1e1b57c52b2bb7136890eb4
ueg1990 <uehtesham90@gmail.com>
2015-01-19 18:29:47 -0500

    Add option tu run piped commands

--- twilix/cmd.py
+++ twilix/cmd.py
@@ -9,28 +9,35 @@ def cmd_ls(*args):
     return subprocess.check_output(*args)
 
 def cmd_cd(*args):
-    if path[0] == '~':
-        path[0] = os.path.expanduser(path[0])
-    os.chdir(path[0])
-    return run_pwd()
+    if args[0][1] == '~':
+        args[0][1] = os.path.expanduser(args[0][1])
+    os.chdir(args[0][1])
+    return cmd_pwd()
 
 def cmd_mkdir(*args):
     try:
-        if path[0][0] == '~':
-            path[0] = os.path.expanduser(path[0])
-	os.makedirs(path[0])
-        return "Director {0} created".format(path)
+        if args[0][1][0] == '~':
+            args[0][1] = os.path.expanduser(args[0][1])
+	os.makedirs(args[0][1])
+        return "Director {0} created".format(args[0][1])
     except OSError as exception:
         if exception.errno != errno.EEXIST:
 	    raise
 
+def cmd_pipe(*args):
+    p1 = subprocess.Popen(args[0][0], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(args[0][1], stdin=p1.stdout, stdout=subprocess.PIPE)
+    output = p2.communicate()[0]
+    return output
+ 
 cmds = {
     'pwd' : cmd_pwd,
     'ls'  : cmd_ls,
     'cd'  : cmd_cd,
-    'mkdir': cmd_mkdir
+    'mkdir': cmd_mkdir,
+    'pipe': cmd_pipe
 }
 
 if __name__ == '__main__':
-    a = cmd_mkdir("~/Test/ing")
+    a = cmd_mkdir("~/ue/mhacks")
     print a
